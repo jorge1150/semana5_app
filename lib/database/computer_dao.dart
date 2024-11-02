19abb16c59848b4c9afe588f929f7b145147675f
Jorge Mosquera <jorgemosquera1150@gmail.com>
2024-11-01 23:32:03 -0500

    Se elimina duplicidad

--- lib/database/computer_dao.dart
+++ lib/database/computer_dao.dart
@@ -1,39 +1,19 @@
 import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 import '../models/computer_model.dart';
+import 'database_helper.dart';
 
 class ComputerDao {
   static final ComputerDao _instance = ComputerDao._internal();
-  static Database? _database;
-
-  factory ComputerDao() {
-    return _instance;
-  }
-
+  factory ComputerDao() => _instance;
   ComputerDao._internal();
 
   Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDatabase();
-    return _database!;
-  }
-
-  Future<Database> _initDatabase() async {
-    return await openDatabase(
-      join(await getDatabasesPath(), 'computers.db'),
-      onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE computers(id INTEGER PRIMARY KEY AUTOINCREMENT, processor TEXT, hardDrive TEXT, ram TEXT)',
-        );
-      },
-      version: 1,
-    );
+    return await DatabaseHelper().database;
   }
 
   Future<List<Computer>> fetchComputers() async {
     final db = await database;
     final List<Map<String, dynamic>> maps = await db.query('computers');
-
     return List.generate(maps.length, (i) {
       return Computer(
         id: maps[i]['id'],
